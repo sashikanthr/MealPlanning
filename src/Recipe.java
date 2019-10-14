8f9c618ca84691e9c00a11782c769bf2df89d699
eworthy <ellenworthy@gmail.com>
2019-10-14 17:28:36 +0200

    Add start, end, and wait time variables.

--- src/Recipe.java
+++ src/Recipe.java
@@ -6,6 +6,8 @@ public class Recipe {
     private int priority;
     List<Activity> activityList = new ArrayList<>();
     private String name;
+    private int timeStarted, timeEnded;
+    private TimeUnits waitTime;
 
     public int getPriority() {
         return priority;
@@ -29,6 +31,22 @@ public class Recipe {
 
     public List<Activity> getActivities() {
         return activityList;
+    }
+    
+    // time spent between 2 activities in this recipe while we had to wait
+    // for resources to become available
+    public TimeUnits getWaitTime() {
+        return waitTime;
+    }
+    
+    // the time the first activity in this recipe began
+    public int getTimeStarted() {
+        return timeStarted;
+    }
+    
+    // the time the last activity in this recipe was completed
+    public int getTimeEnded() {
+        return timeEnded;
     }
 
     @Override
